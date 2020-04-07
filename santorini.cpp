6e3141822611185b6e1e31e9079c1afcfa6fd4e5
Marco Jonkers <contact@marcojonkers.com>
2020-04-07 20:49:27 +0200

    Santorini: Add building and win condition

--- santorini.cpp
+++ santorini.cpp
@@ -1,6 +1,7 @@
 #include "santorini.h"
 #include "imgui.h"
 #include "mj_common.h"
+#include <stdio.h>
 
 struct Pos
 {
@@ -19,27 +20,38 @@ struct Player
   Pos* pWorkers;
 };
 
+// 0 1 2
+// 3   4
+// 5 6 7
 enum EDirection
 {
   EDirection_NorthWest,
   EDirection_North,
   EDirection_NorthEast,
-  EDirection_East,
-  EDirection_SouthEast,
-  EDirection_South,
-  EDirection_SouthWest,
   EDirection_West,
+  EDirection_East,
+  EDirection_SouthWest,
+  EDirection_South,
+  EDirection_SouthEast,
   EDirectionCount
 };
 
 struct MoveMask
 {
-  // bool CanMove[EDirectionCount];
   int spaces[EDirectionCount];
   int numSpaces;
 };
 
-static Player** s_Players;
+struct Square
+{
+  int height = 0;
+  bool dome  = false;
+  int player = -1;
+  int worker = -1;
+};
+
+static Player** s_ppPlayers;
+static Square* s_pBoard;
 
 bool operator==(const Pos& lhs, const Pos& rhs)
 {
@@ -66,13 +78,56 @@ static uint32_t Xorshift32()
 static int s_NumRows    = 5;
 static int s_NumColumns = 5;
 
+static void NewGame()
+{
+  const int numSquares = s_NumRows * s_NumColumns;
+
+  new (s_pBoard) Square[numSquares];
+
+  if (numSquares >= s_NumPlayers * s_NumWorkers)
+  {
+    mj::IStream stream = s_TransientMemory;
+    int* const pPositions  = stream.ReserveArrayUnaligned<int>(numSquares);
+    if (pPositions)
+    {
+      // Random worker positions
+      for (int i = 0; i < numSquares; i++)
+      {
+        pPositions[i] = i;
+      }
+
+      int spacesLeft = numSquares;
+      for (int i = 0; i < s_NumPlayers; i++)
+      {
+        for (int j = 0; j < s_NumWorkers; j++)
+        {
+          Pos& pos  = s_ppPlayers[i]->pWorkers[j];
+          int index = Xorshift32() % spacesLeft;
+          int space = pPositions[index];
+          pos.x     = space % s_NumColumns;
+          pos.y     = space / s_NumColumns;
+
+          Square& sq = s_pBoard[pos.y * s_NumColumns + pos.x];
+          sq.player  = i;
+          sq.worker  = j;
+
+          spacesLeft--;
+          mj::swap(pPositions[index], pPositions[spacesLeft]);
+        }
+      }
+    }
+  }
+}
+
 void SantoriniInit()
 {
   mj::IStream stream(s_Memory, sizeof(s_Memory));
-  s_Players = nullptr;
+  s_ppPlayers = nullptr;
+  s_pBoard   = nullptr;
 
-  Player** ppPlayers = stream.ReserveArrayUnaligned<Player*>(s_NumPlayers);
-  if (ppPlayers)
+  decltype(s_ppPlayers) ppPlayers = stream.ReserveArrayUnaligned<Player*>(s_NumPlayers);
+  decltype(s_pBoard) pBoard      = stream.NewArrayUnaligned<Square>(s_NumRows * s_NumColumns);
+  if (ppPlayers && pBoard)
   {
     for (int i = 0; i < s_NumPlayers; i++)
     {
@@ -85,10 +140,13 @@ void SantoriniInit()
     if (stream.Good())
     {
       s_TransientMemory = stream;
-      s_Players         = ppPlayers;
+      s_ppPlayers         = ppPlayers;
+      s_pBoard           = pBoard;
       s_CurrentPlayer   = 0;
     }
   }
+
+  NewGame();
 }
 
 static inline bool OnBoard(int x, int y)
@@ -98,18 +156,54 @@ static inline bool OnBoard(int x, int y)
 
 static MoveMask GetMoveMask(int player, int worker)
 {
-  Pos& pos = s_Players[player]->pWorkers[worker];
+  Pos& pos     = s_ppPlayers[player]->pWorkers[worker];
+  Square& from = s_pBoard[pos.y * s_NumColumns + pos.x];
 
-  MoveMask mask;
+  MJ_UNINITIALIZED MoveMask mask;
   mask.numSpaces = 0;
-  for (int x = pos.x - 1; x <= pos.x + 1; x++)
+  for (int y = pos.y - 1; y <= pos.y + 1; y++)
   {
-    for (int y = pos.y - 1; y <= pos.y + 1; y++)
+    for (int x = pos.x - 1; x <= pos.x + 1; x++)
     {
-      if (OnBoard(x, y))
+      if (!((x == pos.x) && (x == pos.y))) // Don't move in place
       {
-        int space                     = y * s_NumColumns + x;
-        mask.spaces[mask.numSpaces++] = space;
+        if (OnBoard(x, y))
+        {
+          int sq     = y * s_NumColumns + x;
+          Square& to = s_pBoard[sq];
+          if (!to.dome && ((to.height - from.height) <= 1) && (to.player == -1))
+          {
+            // Add to moveset
+            mask.spaces[mask.numSpaces++] = sq;
+          }
+        }
+      }
+    }
+  }
+
+  return mask;
+}
+
+static MoveMask GetBuildMask(const Pos& pos)
+{
+  MJ_UNINITIALIZED MoveMask mask;
+  mask.numSpaces = 0;
+  for (int y = pos.y - 1; y <= pos.y + 1; y++)
+  {
+    for (int x = pos.x - 1; x <= pos.x + 1; x++)
+    {
+      if (!((x == pos.x) && (x == pos.y))) // Don't move in place
+      {
+        if (OnBoard(x, y))
+        {
+          int sq     = y * s_NumColumns + x;
+          Square& to = s_pBoard[sq];
+          if (!to.dome && (to.player == -1))
+          {
+            // Add to moveset
+            mask.spaces[mask.numSpaces++] = sq;
+          }
+        }
       }
     }
   }
@@ -123,12 +217,53 @@ static void MakeMove()
   int worker        = Xorshift32() % s_NumWorkers;
   MoveMask moveMask = GetMoveMask(s_CurrentPlayer, worker);
 
-  // Pick random move
-  int index = Xorshift32() % moveMask.numSpaces;
-  int space = moveMask.spaces[index];
-  Pos& pos  = s_Players[s_CurrentPlayer]->pWorkers[worker];
-  pos.x     = space % s_NumColumns;
-  pos.y     = space / s_NumColumns;
+  if (moveMask.numSpaces > 0)
+  {
+    // Pick random move
+    int index    = Xorshift32() % moveMask.numSpaces;
+    int space    = moveMask.spaces[index];
+    Pos& pos     = s_ppPlayers[s_CurrentPlayer]->pWorkers[worker];
+    Square& from = s_pBoard[pos.y * s_NumColumns + pos.x];
+    pos.x        = space % s_NumColumns;
+    pos.y        = space / s_NumColumns;
+
+    // Move player on board
+    Square& to = s_pBoard[space];
+    mj::swap(to.player, from.player);
+    mj::swap(to.worker, from.worker);
+
+    if (to.height == 3)
+    {
+      printf("Player %d wins!\n", s_CurrentPlayer + 1);
+    }
+    else
+    {
+      // Random build
+      MoveMask buildMask = GetBuildMask(pos);
+      if (buildMask.numSpaces > 0)
+      {
+        index      = Xorshift32() % buildMask.numSpaces;
+        space      = buildMask.spaces[index];
+        Square& sq = s_pBoard[space];
+        if (sq.height == 3)
+        {
+          sq.dome = true;
+        }
+        else
+        {
+          sq.height++;
+        }
+      }
+      else
+      {
+        printf("P%d.%d cannot build!\n", s_CurrentPlayer + 1, worker + 1);
+      }
+    }
+  }
+  else
+  {
+    printf("P%d.%d cannot move!\n", s_CurrentPlayer + 1, worker + 1);
+  }
 
   // End turn
   s_CurrentPlayer++;
@@ -152,69 +287,41 @@ void SantoriniUpdate()
     SantoriniInit();
   }
 
-  if (ImGui::Button("New Game") && s_Players)
+  if (ImGui::Button("New Game") && s_ppPlayers)
   {
-    const int numSlots = s_NumRows * s_NumColumns;
-    if (numSlots >= s_NumPlayers * s_NumWorkers)
-    {
-      mj::IStream stream = s_TransientMemory;
-      int* const pSlots  = stream.ReserveArrayUnaligned<int>(numSlots);
-      if (pSlots)
-      {
-        // Random worker positions
-        for (int i = 0; i < numSlots; i++)
-        {
-          pSlots[i] = i;
-        }
-
-        int spacesLeft = numSlots;
-        for (int i = 0; i < s_NumPlayers; i++)
-        {
-          for (int j = 0; j < s_NumWorkers; j++)
-          {
-            Pos& pos  = s_Players[i]->pWorkers[j];
-            int index = Xorshift32() % spacesLeft;
-            int space = pSlots[index];
-            pos.x     = space % s_NumColumns;
-            pos.y     = space / s_NumColumns;
-            spacesLeft--;
-            mj::swap(pSlots[index], pSlots[spacesLeft]);
-          }
-        }
-      }
-    }
+    NewGame();
   }
 
-  if (ImGui::Button("Step") && s_Players)
+  if (ImGui::Button("Step") && s_ppPlayers)
   {
     MakeMove();
   }
 
   ImGui::Columns(s_NumColumns);
 
-  for (int i = 0; i < s_NumRows; i++)
+  if (s_pBoard)
   {
-    for (int j = 0; j < s_NumColumns; j++)
+    for (int y = 0; y < s_NumRows; y++)
     {
-      ImGui::Text("%d", 0);
-      const Pos pos(j, i);
-
-      if (s_Players)
+      for (int x = 0; x < s_NumColumns; x++)
       {
-        for (int k = 0; k < s_NumPlayers; k++)
-        {
-          for (int m = 0; m < s_NumWorkers; m++)
-          {
-            if (pos == s_Players[k]->pWorkers[m])
-            {
-              ImGui::SameLine();
-              ImGui::Text("P%d.%d", k + 1, m + 1);
-            }
-          }
-        }
-      }
+        Square& sq = s_pBoard[y * s_NumColumns + x];
 
-      ImGui::NextColumn();
+        ImGui::Text("%d", sq.height);
+        if (sq.dome)
+        {
+          ImGui::SameLine();
+          ImGui::Text("!");
+        }
+
+        if (sq.player >= 0)
+        {
+          ImGui::SameLine();
+          ImGui::Text("P%d.%d", sq.player + 1, sq.worker + 1);
+        }
+
+        ImGui::NextColumn();
+      }
     }
   }
 
